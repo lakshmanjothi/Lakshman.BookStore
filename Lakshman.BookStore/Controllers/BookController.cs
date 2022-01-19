221a17b1bfd029e1504171db785472de55490fb4
lakshmanjothi <lakshmanjothi.j@gmail.com>
2022-01-19 14:09:45 +1100

    Add project files.

---
+++ Lakshman.BookStore/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using Lakshman.BookStore.Models;
+using Lakshman.BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lakshman.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository bookrepository=null;
+
+        public BookController()
+        {
+            bookrepository = new BookRepository();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return bookrepository.GetAllBooks();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return bookrepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return bookrepository.SearchBook(bookName,authorName);
+        }
+    }
+}
---
+++ Lakshman.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lakshman.BookStore.Controllers
+{
+    //Inherited from Controller class - using Microsoft.AspNetCore.Mvc;
+    public class HomeController: Controller
+    {
+        //Action method
+        public string Index()
+        {
+            return "Lakshman";
+        }
+    }
+}
