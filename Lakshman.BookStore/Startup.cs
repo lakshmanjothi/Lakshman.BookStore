7d03be9f1d4fde0c25df2b12c80697699b28aaee
lakshmanjothi <lakshmanjothi.j@gmail.com>
2022-02-22 16:27:41 +1100

    Static Files and how to use it

--- Lakshman.BookStore/Startup.cs
+++ Lakshman.BookStore/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,6 +29,16 @@ namespace Lakshman.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
+            //to use user-defined folder
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                //FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory() + "StaticFiles")
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "StaticFiles")),
+
+                RequestPath = "/StaticFiles"
+            });
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
